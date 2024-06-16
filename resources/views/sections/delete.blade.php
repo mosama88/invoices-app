b0c7f562299cea5009e3aa1590377d471721e656
mosama88 <mosama88@hotmail.com>
2024-06-16 15:00:35 +0300

    Delete2

--- resources/views/sections/delete.blade.php
+++ resources/views/sections/delete.blade.php
@@ -3,18 +3,20 @@
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content tx-size-sm">
                     <div class="modal-body tx-center pd-y-20 pd-x-20">
-                        <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button> <i class="icon icon ion-ios-close-circle-outline tx-100 tx-danger lh-1 mg-t-20 d-inline-block"></i>
+                        <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span
+                                aria-hidden="true">&times;</span></button> <i
+                            class="icon icon ion-ios-close-circle-outline tx-100 tx-danger lh-1 mg-t-20 d-inline-block"></i>
                         <h4 class="tx-danger mg-b-20">إحذر: ستقوم بحذف العملية !</h4>
-        
+
                         <form action="{{ route('sections.destroy', $section->id) }}" method="post">
                             @csrf
                             @method('DELETE')
                             <div class="modal-body">
                                 <h5>{{ $section->name }}</h5>
-        
+
                                 <div class="form-group">
-                                    <input type="hidden" name="page_id" value="1" class="form-control"
-                                           id="recipient-name">
+                                    <input type="hidden" name="id" id="id" value="{{ $section->id }}">
+
                                     <input type="hidden" name="id" value="{{ $section->id }}">
                                 </div>
                             </div>
